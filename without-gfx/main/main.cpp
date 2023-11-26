a986c4b0bcbf979a2df14b47aa82c410c3b01b5e
Franz Höpfinger <mail@franzhoepfinger.de>
2023-11-26 14:46:44 +0100

    Add ESP32 FreeRTOS delay and fillRect function
    - Added #include statements for the FreeRTOS library
    - Added a delay of 1 second using vTaskDelay function
    - Added a rectangle filled with a specific color using dma_display->fillRect function

--- without-gfx/main/main.cpp
+++ without-gfx/main/main.cpp
@@ -1,4 +1,7 @@
 #include "ESP32-HUB75-MatrixPanel-I2S-DMA.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
 
 MatrixPanel_I2S_DMA *dma_display = nullptr;
 
@@ -9,4 +12,8 @@ extern "C" void app_main() {
   dma_display->begin();
   dma_display->setBrightness8(80);
   dma_display->clearScreen();
+
+
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+  dma_display->fillRect(20, 20, 30, 25, dma_display->color444(0, 15, 0));
 }
